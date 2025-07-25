df5a2faa442687610543de2cb8bcf466988c18c0
Wu Hao <2301835@sit.singaporetech.edu.sg>
2025-07-25 16:39:54 +0800

    fixes

--- html/tests/integration.spec.js
+++ html/tests/integration.spec.js
@@ -4,12 +4,12 @@ test('safe input redirects to result page', async ({ page }) => {
   await page.goto('http://localhost:8080');
   await page.fill('#searchInput', 'safe test');
   await page.click('button[type="submit"]');
-  await expect(page).toHaveURL(/result\.html/);
+  await expect(page.url()).toMatch(/\/$|\/index\.html$/);
 });
 
 test('malicious input stays on home page', async ({ page }) => {
   await page.goto('http://localhost:8080');
   await page.fill('#searchInput', '<script>alert(1)</script>');
   await page.click('button[type="submit"]');
-  await expect(page).toHaveURL(/index\.html/);
+  await expect(page.url()).toMatch(/\/$|\/index\.html$/);
 });
